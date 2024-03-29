b36792d804d93dfa9fbc820aef3df9a366ddf03b
𓄿𓃭𓎢𓉔𓅂𓅓𓇋𓋴𓏏𓐍 <164961892+Alk1myst@users.noreply.github.com>
2024-03-29 20:27:09 +0000

    Add files via upload

---
+++ name-card-gen.py
@@ -0,0 +1,13 @@
+import sys
+
+#use python3 name-card-gen.py (first name) (last name)
+
+FN = sys.argv[1]
+
+LN = sys.argv[2]
+
+print("_" * 20)
+
+print((FN) + (LN))
+
+print("_" * 20)
---
+++ sys.py
@@ -0,0 +1,3 @@
+import sys
+
+print("This arguement was passed to the script:", sys.argv[1])
